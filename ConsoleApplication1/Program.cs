29933099aa4eb27c1ec0131b242f6af778bd6a7e
chuckrudolph <chuckrudolph@gmail.com>
2023-10-10 08:00:14 -0600

    B2test to b2 (#7)
    via s&m
    
    * added line to b1 after branch from master
    
    * more testing
    
    * Revert "Merge pull request #4 from chuckrudolph/b1"
    
    This reverts commit 03fe59d0e68300043e55226976a0cc0b41de7654, reversing
    changes made to 002c3baa3841bf7a2708fd79d04e3a38d3b49b75.
    
    * push 1
    
    * push 2

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,13 +10,15 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             string path = @"d:\temp\test.txt";
-            int b2 = 2;         // line from b2 off master
 
             System.IO.FileStream fs = new System.IO.FileStream(path, System.IO.FileMode.Open);
             byte[] data = new byte[(int)fs.Length];
             fs.Read(data, 0, (int)fs.Length);
             fs.Close();
             fs.Dispose();
+
+            int b1 = 2;
+            b1 *= 2;
 
             System.Security.Cryptography.SHA1CryptoServiceProvider sha1 = new System.Security.Cryptography.SHA1CryptoServiceProvider();
             byte[] theHash = sha1.ComputeHash(data);
