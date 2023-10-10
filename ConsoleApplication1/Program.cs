87f31ad41349cff07f8c6dda0197f6607e847854
chuckrudolph <chuckrudolph@gmail.com>
2023-10-10 08:22:00 -0600

    add *2

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -19,6 +19,7 @@ namespace ConsoleApplication1
 
             int b1 = 2;
             b1 *= 2;
+            b1 *= 2;
 
             System.Security.Cryptography.SHA1CryptoServiceProvider sha1 = new System.Security.Cryptography.SHA1CryptoServiceProvider();
             byte[] theHash = sha1.ComputeHash(data);
