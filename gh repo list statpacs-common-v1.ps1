9d91439092a7d60893c322ea769ab3960fece0e0
chuckrudolph <chuckrudolph@gmail.com>
2024-12-23 14:42:58 -0700

    Update gh repo list statpacs-common-v1.ps1

--- gh repo list statpacs-common-v1.ps1
+++ gh repo list statpacs-common-v1.ps1
@@ -1,32 +1,43 @@
-$uri = "https://api.github.com/repos/statpacs-common/dicom/commits/b77b36a0b2b53584dd1e1a1fac4cf5df0d3f0270"
+function getCommitInfo([String] $orgName, [String] $repoName, [string] $commitSha){
+#$stuff = gh api /repos/statpacs-common/dicom/commits/b77b36a0b2b53584dd1e1a1fac4cf5df0d3f0270
+#Write-Host $pretty.commit.author.name
+#Write-Host $pretty.commit.author.date
 
-#$stuff = Invoke-RestMethod -uri $uri -Method Get
-$stuff = gh api /repos/statpacs-common/dicom/commits/b77b36a0b2b53584dd1e1a1fac4cf5df0d3f0270
-$pretty = $stuff | ConvertFrom-Json
+$ghCommitInfo =  gh api /repos/$orgName/$repoName/commits/$commitSha | ConvertFrom-Json
 
-Write-Host $pretty.commit.author.name
-Write-Host $pretty.commit.author.date
+$commitInfo = [PSCustomObject]@{
+    Name = $ghCommitInfo.commit.author.name
+    Date = $ghCommitInfo.commit.author.date
+}
+return $commitInfo
+}
 
+$orgList = @("statpacs-common","statpacs-services")
 
-$repolist = gh repo list statpacs-common --json name,url | ConvertFrom-Json
-Write-Host "`nRepos in common:"
+#todo Iterate
+$orgIndex = 0
+
+$repolist = gh repo list $orgList[$orgIndex] --json name,url | ConvertFrom-Json
+Write-Host "`nRepos in $($orgList[$orgIndex]):"
+Write-Host ("-"*35)
+Write-Host $("{0,-35} {1,-25}" -f "Name", "Url")
 $repolist | ForEach-Object {
-    Write-Host "Name: $($_.name), Url:$($_.url)"
+    Write-Host $("{0,-35} {1,-25}" -f $_.name, $_.url)
 }
 
 $repoIndex = 0
 for ($repoIndex = 0; $repoIndex -lt $repolist.Count; $repoIndex++) {
     $branchBlob = git ls-remote --branches $repolist[$repoIndex].url
     $branches = $branchBlob | ConvertFrom-String -PropertyNames id,ref
-    Write-Host "`nBranches in $($repolist[$repoIndex].name)"
+    Write-Host "`n`n$($repolist[$repoIndex].name) Branches"
+    Write-Host ("-"*35)
+    Write-Host $("{0,-35} {1,-25} {2,-25} {3,-35}" -f "Branch", "Last Author", "Date", "Commit Sha")
     $branches | ForEach-Object {
-        Write-Host "id: $($_.id), ref:$($_.ref)"
+        $branchSha = $_.id
+        $branchName = $_.ref -replace "refs/heads/", ""
+        $lastAction = getCommitInfo $orgList[$orgIndex] $repolist[$repoIndex].name $branchSha
+        Write-Host $("{0,-35} {1,-25} {2,-25} {3,-35}" -f $branchName, $lastAction.Name, $lastAction.Date,$branchSha)
     }
+
+    
 }
-
-
-
-
-# $lastPush = gh repo view $($repolist[0].url)  --branch $branches[0].id --json pushedAt,updatedAt,name,owner | ConvertFrom-Json
-# Write-Host "repo view on $($branches[0].ref)"
-# Write-Host ($lastPush | Format-Table | Out-String)
